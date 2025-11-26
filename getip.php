07fb9543114416ad41a95c66abf24f70e8af2bf8
sadiqalishamji <sadiqalishamji@gmail.com>
2025-11-26 14:41:28 +0500

    Update getip.php

--- getip.php
+++ getip.php
@@ -2,3 +2,8 @@
 $ip = file_get_contents('https://api.ipify.org'); // returns your public IPv4
 echo "Public IP: $ip";
 ?>
+
+<?php
+$ip = file_get_contents('https://api64.ipify.org'); // returns IPv6
+echo "Public IP: $ip";
+?>

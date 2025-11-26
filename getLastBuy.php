85c77d97b65e29761659328a12b3b6cc9e3f0d5f
sadiqalishamji <sadiqalishamji@gmail.com>
2025-11-26 12:35:35 +0500

    Initial commit
    Initial commit

---
+++ getLastBuy.php
@@ -0,0 +1,39 @@
+<?php
+// getLastBuy.php
+
+$apiKey = $_SERVER['BINANCE_API_KEY'] ?? 'DUMMY_KEY';
+$apiSecret = $_SERVER['BINANCE_API_SECRET'] ?? 'DUMMY_SECRET';
+
+$symbol = "ETHUSDT";
+$isIsolated = "true";
+$limit = 10; // fetch last 10 trades
+$timestamp = round(microtime(true) * 1000);
+
+$query = "symbol=$symbol&isIsolated=$isIsolated&limit=$limit&timestamp=$timestamp";
+$signature = hash_hmac('sha256', $query, $apiSecret);
+
+$url = "https://api.binance.com/sapi/v1/margin/myTrades?$query&signature=$signature";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-MBX-APIKEY: $apiKey"]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($response, true);
+echo $response;
+$lastBuyPrice = 0;
+if (!empty($data)) {
+    foreach (array_reverse($data) as $trade) {
+        if (isset($trade['isBuyer']) && $trade['isBuyer'] == true) {
+            $lastBuyPrice = $trade['price'];
+            echo $lastBuyPrice;
+            break;
+        }
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode(['lastBuyPrice' => $lastBuyPrice]);
